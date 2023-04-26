2db64ccaaf4763041a6278f0c1ee89d66448dfd6
rzymski <83671265+rzymski@users.noreply.github.com>
2023-04-27 01:38:08 +0200

    chyba dobrze teraz moze

--- PrzestawieniaMacierzowe/Program.cs
+++ PrzestawieniaMacierzowe/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PrzestawieniaMacierzowe
@@ -311,6 +313,109 @@ namespace PrzestawieniaMacierzowe
             return result;
         }
 
+        public static string encryptBCorrect(string text, string key)
+        {
+            //usuniecie spacji z wiadomosci
+            text = text.Replace(" ", "");
+            //ustalenie porządku klucza
+            int[] orderKey = new int[key.Length];
+            int indx = 1;
+            for (int i = 65; i <= 122; i++)
+            {
+                for (int j = 0; j < key.Length; j++)
+                {
+                    if (key[j] == (char)i)
+                        orderKey[j] = indx++;
+                }
+            }
+            //wypisanie klucza
+            //Console.WriteLine("Order:");
+            //for(int i=0; i<orderKey.Length; i++)
+            //    Console.Write($"{orderKey[i],3}");
+            //zadklerowanie tablic
+            int matrixRows = text.Length / orderKey.Length + 1;
+            char[][] matrix = new char[matrixRows][];
+            for (int i = 0; i < matrixRows; i++)
+                matrix[i] = new char[orderKey.Length];
+            //uzupełnienie macierzy
+            indx = 0;
+            for (int i = 0; i < matrixRows; i++)
+                for (int j = 0; j < orderKey.Length; j++)
+                    if (indx < text.Length)
+                        matrix[i][j] = text[indx++];
+                    else
+                        matrix[i][j] = '\0';
+            //wypisanie tablic
+            //Console.WriteLine();
+            //for (int i = 0; i < matrixRows; i++)
+            //{
+            //    for (int j = 0; j < orderKey.Length; j++)
+            //        Console.Write($"{matrix[i][j],3}");
+            //    Console.WriteLine();
+            //}
+            //odczytanie wyniku z macierzy
+            string result = "";
+            for (int i = 1; i <= orderKey.Length; i++)
+            {
+                int column = Array.IndexOf(orderKey, i); //orderKey[i];
+                for (int j = 0; j < matrixRows; j++)
+                {
+                    //if (matrix[j][column] != '\0')
+                    //    result += matrix[j][column];
+                    //else
+                    //    result += "_";
+                    result += matrix[j][column];
+                }
+            }
+            return result;
+        }
+
+        public static string decryptBCorrect(string text, string key)
+        {
+            int width = key.Length;
+            int height = text.Length / key.Length;
+            //ustalenie porządku klucza
+            int[] orderKey = new int[width];
+            int indx = 1;
+            for (int i = 65; i <= 122; i++)
+                for (int j = 0; j < width; j++)
+                    if (key[j] == (char)i)
+                        orderKey[j] = indx++;
+            //wypisanie klucza
+            //Console.WriteLine("Order:");
+            //for (int i = 0; i < orderKey.Length; i++)
+            //    Console.Write($"{orderKey[i],3}");
+            //stworzenie macierzy
+            char[][] matrix = new char[height][];
+            for(int i = 0;i < height;i++)
+                matrix[i] = new char[width];
+            //uzupełnienie macierzy
+            //Console.WriteLine();
+            for (int i = 0, m = 0; i < width; i++)
+            {
+                int k = Array.IndexOf(orderKey, i + 1);
+                for (int j = 0; j < height && m < text.Length; j++)
+                {
+                    //Console.WriteLine("I: " + i + "  J: " + j + "   Text: " + text[m]);
+                    matrix[j][k] = text[m++];
+                }
+            }
+            //wypisanie macierzy
+            //Console.WriteLine();
+            //for (int i = 0; i < height; i++)
+            //{
+            //    for (int j = 0; j < width; j++)
+            //        Console.Write($"{matrix[i][j],3}");
+            //    Console.WriteLine();
+            //}
+            //zapisanie wyniku
+            string result = "";
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    result += matrix[i][j];
+            return result;
+        }
+
         static void Main(string[] args)
         {
             //dodatkowe przykłady 2a
@@ -329,26 +434,20 @@ namespace PrzestawieniaMacierzowe
 
             //dodatkowe przykłady 2b
             Console.WriteLine("\nPrzykłady B");
-            Console.WriteLine(encryptB("HERE IS A SECRET MESSAGE ENCIPHERED BY TRANSPOSITION", "CONVENIENCE"));
-            //Console.WriteLine(decryptB("HECRN CEYI ISEP SGDI RNTO AAES RMPN SSRO EEBT ETIA EEHS", "CONVENIENCE"));
-            //Console.WriteLine(decryptB(encryptB("HERE IS A SECRET MESSAGE ENCIPHERED BY TRANSPOSITION", "CONVENIENCE"), "CONVENIENCE"));
+            Console.WriteLine(encryptBCorrect("HERE IS A SECRET MESSAGE ENCIPHERED BY TRANSPOSITION", "CONVENIENCE"));
+            Console.WriteLine(decryptBCorrect(encryptBCorrect("HERE IS A SECRET MESSAGE ENCIPHERED BY TRANSPOSITION", "CONVENIENCE"), "CONVENIENCE"));
 
-            Console.WriteLine(encryptB("MyPasswordIsPassword", "CONVENIENCE"));
-            //Console.WriteLine(decryptB("HECRN CEYI ISEP SGDI RNTO AAES RMPN SSRO EEBT ETIA EEHS", "CONVENIENCE"));
-            Console.WriteLine(decryptB(encryptB("MyPasswordIsPassword", "CONVENIENCE"), "CONVENIENCE"));
+            Console.WriteLine("\n"+ encryptBCorrect("FRANCJA", "CDAEB"));
+            Console.WriteLine(decryptBCorrect(encryptBCorrect("FRANCJA", "CDAEB"), "CDAEB"));
 
-            Console.WriteLine("\n"+encryptB("FRANCJA", "CDAEB"));
-            Console.WriteLine(decryptB("A C FJ RA N", "CDAEB"));
-            Console.WriteLine(decryptB(encryptB("FRANCJA", "CDAEB"), "CDAEB"));
-            Console.WriteLine(encryptB("DOBRYDZIEN", "CDAEB"));
-            Console.WriteLine(decryptB("BI YN DD OZ RE", "CDAEB"));
-            Console.WriteLine(decryptB(encryptB("DOBRYDZIEN", "CDAEB"), "CDAEB"));
-            Console.WriteLine(encryptB("POLITECHNIKA", "CDAEB"));
-            Console.WriteLine(decryptB("LH TI PEK OCA IN", "CDAEB"));
-            Console.WriteLine(decryptB(encryptB("POLITECHNIKA", "CDAEB"), "CDAEB"));
-            Console.WriteLine(encryptB("UNIVERSITY", "CDAEB"));
-            Console.WriteLine(decryptB("II EY UR NS VT", "CDAEB"));
-            Console.WriteLine(decryptB(encryptB("UNIVERSITY", "CDAEB"), "CDAEB"));
+            Console.WriteLine("\n" + encryptBCorrect("DOBRYDZIEN", "CDAEB"));
+            Console.WriteLine(decryptBCorrect(encryptBCorrect("DOBRYDZIEN", "CDAEB"), "CDAEB"));
+
+            Console.WriteLine("\n" + encryptBCorrect("POLITECHNIKA", "CDAEB"));
+            Console.WriteLine(decryptBCorrect(encryptBCorrect("POLITECHNIKA", "CDAEB"), "CDAEB"));
+
+            Console.WriteLine("\n" + encryptBCorrect("UNIVERSITY", "CDAEB"));
+            Console.WriteLine(decryptBCorrect(encryptBCorrect("UNIVERSITY", "CDAEB"), "CDAEB"));
         }
     }
 }
